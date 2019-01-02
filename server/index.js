078f40b8369bcc3c2a281725f8aef10284678621
Zexi Han <zexihan@me.com>
2019-01-02 18:45:38 +0800

    adding mongodb

--- server/index.js
+++ server/index.js
@@ -1,37 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
+require('./models/User')
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-// console.developers.google.com
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
-        }, 
-        (accessToken, refreshToken, profile, done) => {
-            console.log('access token', accessToken);
-            console.log('refresh token', refreshToken);
-            console.log('profile', profile);
-        }
-    )
-);
-
-app.get(
-    '/auth/google', 
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
     })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get(
-    '/auth/google/callback', 
-    passport.authenticate('google')
-);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
